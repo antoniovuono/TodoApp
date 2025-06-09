a22a95607f84544067b662ea0a3be7b41cd7cf69
Antonio Vuono <antoniosvuono@icloud.com>
2025-06-09 17:10:26 -0300

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Antonio Vuono on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
