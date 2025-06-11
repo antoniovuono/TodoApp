df5d0f9fabd85ccbbb4bcc2692de52683db13bac
Antonio Vuono <antoniosvuono@icloud.com>
2025-06-11 18:06:21 -0300

    feature: add task form created

--- todoList/Home.swift
+++ todoList/Home.swift
@@ -18,9 +18,21 @@ struct Home: View {
                 HStack() {
                     TextField("Adicione uma nova tarefa", text: $taskTitle)
                         .textInputAutocapitalization(.never)
-                        .textFiel
-                       
+                        .padding(16)
+                        .background(.gray500)
+                        .foregroundStyle(.gray100)
+                        .clipShape(.rect(cornerRadius: 8))
+                    
+                    
+                    Button(action: {}) {
+                        Text("Adicionar")
+                            .padding(16)
+                            .background(.blueDark)
+                            .foregroundStyle(.gray100)
+                            .clipShape(.rect(cornerRadius: 8))
+                    }
                 }
+                .padding(.horizontal, 24)
                 
                 Spacer()
             }
