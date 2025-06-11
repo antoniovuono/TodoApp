9ac4a4af5cf4dda39387608624000f19cda00cc1
Antonio Vuono <antoniosvuono@icloud.com>
2025-06-10 21:42:37 -0300

    chore: app uploded on github

--- todoList/Home.swift
+++ todoList/Home.swift
@@ -1,15 +1,30 @@
-//
-//  Home.swift
-//  todoList
-//
-//  Created by Antonio Vuono on 09/06/25.
-//
-
 import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        @State var taskTitle: String = ""
+        
+        ZStack {
+            Color.gray600
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Image("logo")
+                }
+                .frame(maxWidth: .infinity, maxHeight: 130)
+                .background(Color.gray700)
+                
+                HStack() {
+                    TextField("Adicione uma nova tarefa", text: $taskTitle)
+                        .textInputAutocapitalization(.never)
+                        .textFiel
+                       
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
